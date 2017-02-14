fb832fb1ade78c548f142c63ac250c5269e09917
sysamir <alekberov.samir@mail.ru>
2017-02-14 13:36:50 +0400

    ok2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\BlogNews;
 use App\BlogCategories;
+use App\Tender;
+use App\Categories;
 
 class HomeController extends Controller
 {
@@ -22,6 +24,10 @@ class HomeController extends Controller
     public function index()
     {
         $blog = BlogNews::with('blogcat')->orderBy('blog_id','desc')->limit(3)->get();
-        return view('client.index',compact('blog'));
+        $tenderSearch = Tender::with('category','username','person','company')->orderBy('tender_id','DESC')->get();
+        $tenderCATE = Categories::with('parent','children')->whereNull('cat_parent')->get();
+        $tenderindex = Tender::with('category','username','person','company')->where('tender_status',1)->orderBy('tender_id','DESC')->paginate(4);
+        return view('client.index',compact('blog','tenderSearch','tenderCATE','tenderindex'));
+
     }
 }
